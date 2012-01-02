4c44ab353f0f3f6daf81c9e4f0f4479f8cbe1878
IOhannes m zmoelnig (gpg-key at iem) <zmoelnig@iem.at>
2012-01-02 18:57:43 +0100

    0.5.0-1 (patches unapplied)
    Imported using git-ubuntu import.

---
+++ update_changelog.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+####################################
+# changelog generator using git-dch
+
+CHANGELOG=ChangeLog
+
+
+getcurrentversion () {
+  dpkg-parsechangelog --count 1 -l${CHANGELOG} |  egrep "^Version:" | head -1 | cut -f2 -d' '
+}
+
+if [ "x$2" = "x" ]; then
+## guess current version
+ NEWVERSION=$1
+ OLDVERSION=$(getcurrentversion)
+else
+ OLDVERSION=$1
+ NEWVERSION=$2
+fi
+
+if git tag -l v${OLDVERSION} | grep . >/dev/null
+then
+ :
+else
+ echo "it seems like there is no tag 'v${OLDVERSION}'" 1>&2
+ exit 1
+fi
+
+if [ "x$OLDVERSION" = "x" ]; then
+ echo "usage: $0 [<LASTVERSION>] <CURVERSION>" 1>&2
+ exit 1
+fi
+
+echo "updating from $OLDVERSION"
+
+if [ "x$NEWVERSION" = "x" ]; then
+ echo "usage: $0 [<LASTVERSION>] <CURVERSION>" 1>&2
+ exit 1
+fi
+
+if dpkg --compare-versions ${OLDVERSION} ge ${NEWVERSION}
+then
+ echo "version mismatch: $OLDVERSION is newer than $NEWVERSION" 1>&2
+ exit 1
+fi
+
+echo "updating to $NEWVERSION"
+
+OK=false
+mkdir debian 
+cp ${CHANGELOG} debian/changelog
+git-dch --since "v${OLDVERSION}" -N ${NEWVERSION} && cat debian/changelog > ${CHANGELOG} && OK=true
+rm -rf debian
+
+if [ "x$OK" = "xtrue" ]; then
+ echo "all went well"
+ echo "check your $CHANGELOG and don't forget to git-tag the new version as v${NEWVERSION}"
+fi
